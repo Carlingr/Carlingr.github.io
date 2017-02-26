3b372d109b30283fa621cb42d1b1416c11300e5d
Carlingr <kittyclubco@gmail.com>
2017-02-26 12:24:18 -0500

    fixes

--- script/layout.js
+++ script/layout.js
@@ -11,11 +11,22 @@ var navbar; //holds navbar objects
 var header; //holds header
 
 function setup() {
+  var i; //declare a variable, easier than declaring for each loop
+  if (windowWidth < 740) { //if the window is "narrow"
+    for (i = 0; i < aside.length; i++) { //hide all aside elements
+      aside[i].hide;
+    }
+    nav.style('width', "100%"); //make things full width
+    for (i = 0; i < article.length; i++) {
+      article[i].style('width', "100%"); //make things full width
+    }
+  }
+
   nav = select("nav"); //selects allnav elements
   nav.html('<table id="navbar"> <tr> <td class="navbar"><a href="https://carlingr.github.io/prog" class="navlnk">Programming</a></td>        <td class="navbar"><a href="https://carlingr.github.io/electric" class="navlnk">Electronic Prototyping</a></td>        <td class="navbar"><a href="https://carlingr.github.io/theater" class="navlnk">Theater Tech</a></td>        <td class="navbar"><a href="https://carlingr.github.io/video" class="navlnk">Video and Audio Editing</a></td>        <td class="navbar"><a href="https://carlingr.github.io/MacGyverIRL" class="navlnk">MacGyver IRL</a></td>        <td class="navbar"><a href="https://carlingr.github.io/contact" class="navlnk">Get in Touch</a></td>      </tr>    </table>') //makes consistant navbar
 
   navbar = selectAll('.navbar'); //grab all the navbar objects
-  for (var i = 0; i < navbar.length; i++) {
+  for (i = 0; i < navbar.length; i++) {
     navbar[i].mouseOver(over); //highlight on mouse over
     navbar[i].mouseOut(out); //unhighlight on mouse out
   }
@@ -23,8 +34,8 @@ function setup() {
   aside = selectAll('aside'); //selects all aside elements
   article = selectAll("article"); //selects all article elements
 
-header = select("header");//grab the header
-header.html('<br/><a href="https://carlingr.github.io" class="subt">carlingr.github.io</a>',true)//add this strng to the end
+  header = select("header"); //grab the header
+  header.html('<br/><a href="https://carlingr.github.io" class="subt">carlingr.github.io</a>', true) //add this strng to the end
 }
 
 function windowResized() { //if the window is resized

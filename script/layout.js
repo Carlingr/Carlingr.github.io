32d5d2b2c4a21c3542a9b57c182dde22e2c4fd68
carlingr <kittyclubco@gmail.com>
2017-03-21 09:04:48 -0400

    bugfix
    fixed bug where select was below access of object

--- script/layout.js
+++ script/layout.js
@@ -13,31 +13,36 @@ var header; //holds header
 function setup() {
   var i; //declare a variable, easier than declaring for each loop
   if (windowWidth < 740) { //if the window is "narrow"
+    aside = selectAll('aside'); //selects all aside elements
     if (aside) {
       for (i = 0; i < aside.length; i++) { //hide all aside elements
         aside[i].hide;
       }
     }
-    nav.style('width', "100%"); //make things full width
-    for (i = 0; i < article.length; i++) {
-      article[i].style('width', "100%"); //make things full width
+
+
+    header = select("header"); //grab the header
+    header.html('<br/><a href="https://carlingr.github.io" class="subt">carlingr.github.io</a>', true) //add this strng to the end
+
+    nav = select("nav"); //selects allnav elements
+    nav.html('<table id="navbar"> <tr> <td class="navbar"><a href="https://carlingr.github.io/prog" class="navlnk">Programming</a></td>        <td class="navbar"><a href="https://carlingr.github.io/electric" class="navlnk">Electronic Prototyping</a></td>        <td class="navbar"><a href="https://carlingr.github.io/theater" class="navlnk">Theater Tech</a></td>        <td class="navbar"><a href="https://carlingr.github.io/video" class="navlnk">Video and Audio Editing</a></td>        <td class="navbar"><a href="https://carlingr.github.io/MacGyverIRL" class="navlnk">MacGyver IRL</a></td>        <td class="navbar"><a href="https://carlingr.github.io/contact" class="navlnk">Get in Touch</a></td>      </tr>    </table>') //makes consistant navbar
+    if (nav) {
+      nav.style('width', "100%"); //make things full width
+    }
+
+    navbar = selectAll('.navbar'); //grab all the navbar objects
+    for (i = 0; i < navbar.length; i++) {
+      navbar[i].mouseOver(over); //highlight on mouse over
+      navbar[i].mouseOut(out); //unhighlight on mouse out
+    }
+
+    article = selectAll("article"); //selects all article elements
+    if (article) {
+      for (i = 0; i < article.length; i++) {
+        article[i].style('width', "100%"); //make things full width
+      }
     }
   }
-
-  nav = select("nav"); //selects allnav elements
-  nav.html('<table id="navbar"> <tr> <td class="navbar"><a href="https://carlingr.github.io/prog" class="navlnk">Programming</a></td>        <td class="navbar"><a href="https://carlingr.github.io/electric" class="navlnk">Electronic Prototyping</a></td>        <td class="navbar"><a href="https://carlingr.github.io/theater" class="navlnk">Theater Tech</a></td>        <td class="navbar"><a href="https://carlingr.github.io/video" class="navlnk">Video and Audio Editing</a></td>        <td class="navbar"><a href="https://carlingr.github.io/MacGyverIRL" class="navlnk">MacGyver IRL</a></td>        <td class="navbar"><a href="https://carlingr.github.io/contact" class="navlnk">Get in Touch</a></td>      </tr>    </table>') //makes consistant navbar
-
-  navbar = selectAll('.navbar'); //grab all the navbar objects
-  for (i = 0; i < navbar.length; i++) {
-    navbar[i].mouseOver(over); //highlight on mouse over
-    navbar[i].mouseOut(out); //unhighlight on mouse out
-  }
-
-  aside = selectAll('aside'); //selects all aside elements
-  article = selectAll("article"); //selects all article elements
-
-  header = select("header"); //grab the header
-  header.html('<br/><a href="https://carlingr.github.io" class="subt">carlingr.github.io</a>', true) //add this strng to the end
 }
 
 function windowResized() { //if the window is resized
